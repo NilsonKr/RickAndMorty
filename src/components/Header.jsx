5986e73b7638685736eab663eac4086451311af1
NilsonKr <nuotrok444@gmail.com>
2021-03-11 17:54:32 -0500

    :tada: [INITIAL] Structure Base

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import HeaderIcon from "../assets/icon.svg"
+import './styles/Header.css'
+
+function Header(){
+  return (
+    <React.Fragment>
+      <header className="header">
+        <div className="header__home">
+          <img src={HeaderIcon} alt="Icon Header"/>
+        </div>
+        <div  className="header__details">
+          <ul>
+            <li className="details--item">
+              <a href="/">Contact</a>
+            </li>
+            <li className="details--item">
+              <a href="https://rickandmortyapi.com/" target="_blank">Api</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+    </React.Fragment>
+  )
+}
+
+export default Header
