0069c1605afe4a3fddebc5e04368df7295726548
NilsonKr <nuotrok444@gmail.com>
2021-03-28 22:50:37 -0500

    :sparkles: [ADD] Toggle Btn Theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HeaderIcon from "../assets/icon.svg"
-import './styles/Header.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import HeaderIcon from '../assets/icon.svg';
+import './styles/Header.css';
 
+function Header() {
+	const [theme, setTheme] = useState('light');
 
-function Header(){
-  return (
-    <React.Fragment>
-      <header className="header">
-        <Link to="/" className="header__home">
-          <img src={HeaderIcon} alt="Icon Header"/>
-        </Link>
-        <div  className="header__details">
-          <ul>
-            <li className="details--item">
-              <Link to="/contact/">Contact</Link>
-            </li>
-            <li className="details--item">
-              <a href="https://rickandmortyapi.com/" target="_blank" rel="noreferrer" >Api</a>
-            </li>
-          </ul>
-        </div>
-      </header>
-
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<header className='header'>
+				<Link to='/' className='header__home'>
+					<img src={HeaderIcon} alt='Icon Header' />
+				</Link>
+				<div className='header__details'>
+					<ul>
+						<li className='details--item'>
+							<Link to='/contact/'>Contact</Link>
+						</li>
+						<li className='details--item'>
+							<a href='https://rickandmortyapi.com/' target='_blank' rel='noreferrer'>
+								Api
+							</a>
+						</li>
+					</ul>
+					<label className='header__switch'>
+						<input type='checkbox' />
+						<span className='switch__slideBar'></span>
+					</label>
+				</div>
+			</header>
+		</React.Fragment>
+	);
 }
 
-export default Header
+export default Header;
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 
-function Layout(props){
-  return (
-    <React.Fragment>
-      <Header />
-      <Hero />
-      {props.children}
-    </React.Fragment>
-  )
+const updateTheme = theme => {
+	if (theme === 'light') {
+		document.body.style.setProperty('--bg', '#fff');
+		document.body.style.setProperty('--font-color', '#000');
+	} else {
+		document.body.style.setProperty('--bg', '#000');
+		document.body.style.setProperty('--font-color', '#fff');
+	}
+};
+
+function Layout(props) {
+	return (
+		<React.Fragment>
+			<Header updateTheme={updateTheme} />
+			<Hero />
+			{props.children}
+		</React.Fragment>
+	);
 }
 
-export default Layout
+export default Layout;
